636c8e7f5dba152467d2d4445bc32a37a4a6f917
cccc <lcc0628@foxmail.com>
2023-01-16 22:42:48 +0800

    feat: 过滤控制台

--- main.js
+++ main.js
@@ -4,15 +4,15 @@ import {
 	createSSRApp
 } from 'vue'
 import store from './store'
+// #ifdef MP
+if (uni.getSystemInfoSync().platform !== "devtools") {
+	console.log = () => {}
+}
+// #endif
 export function createApp() {
 	const app = createSSRApp(App)
 	app.use(store)
 	app.config.globalProperties.$store = store
-	// app.config.globalProperties.$test2 = {
-	// 	playing: false,
-	// 	playTime: 0,
-	// 	formatedPlayTime: '00:00:00'
-	// }
 	return {
 		app
 	}
